060b62726c412712dba093777d24d43771e9efa8
Ellis M <86482196+Quaternim@users.noreply.github.com>
2022-08-14 21:16:14 +0100

    Inital Commit 🚀

---
+++ main.lua
@@ -0,0 +1,5 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("TITLE", "DarkTheme")
+
+-- Main
+local Tab = Window:NewTab("TabName")
